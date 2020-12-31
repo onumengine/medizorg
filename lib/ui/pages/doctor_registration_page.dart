49bb533a876955713c26c591cbc2626f2bca8aaf
onumengine <onumdev@gmail.com>
2020-12-31 18:29:22 +0100

    Feat(doctor_registration_page.dart):Added a [CircleAvatar]

--- lib/ui/pages/doctor_registration_page.dart
+++ lib/ui/pages/doctor_registration_page.dart
@@ -8,8 +8,34 @@ class _DoctorRegistrationPageState extends State<DoctorRegistrationPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: Text('Register As A Doctor'),
+        child: ListView(
+          children: <Widget>[
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 20,
+            ),
+            CircleAvatar(
+              radius: (MediaQuery.of(context).size.height) / 15,
+              child: Icon(Icons.wallet_travel_sharp),
+              backgroundColor: Colors.blueGrey,
+              foregroundColor: Colors.white,
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                FlatButton(
+                  onPressed: () {},
+                  child: Text('Back'),
+                ),
+                FlatButton(
+                  onPressed: () {},
+                  child: Text('Next'),
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
