9981d5077e347a22adb67e96da9d269f0b0dcc32
onumengine <onumdev@gmail.com>
2021-01-01 18:03:59 +0100

    Feat(health_article.dart):FInished the [HealthArticle] card

--- lib/ui/molecules/health_article.dart
+++ lib/ui/molecules/health_article.dart
@@ -6,58 +6,68 @@ class HealthArticle extends StatelessWidget {
       height: 150.0,
       width: 400.0,
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.all(Radius.circular(8.0)),
+        borderRadius: BorderRadius.all(Radius.circular(10.0)),
         color: Colors.blue,
       ),
       child: Padding(
         padding: const EdgeInsets.all(12.0),
-        child: Column(
+        child: Row(
           children: <Widget>[
-            Align(
-              alignment: AlignmentDirectional.topStart,
-              child: Text('01 Jan 2021'),
-            ),
-            Align(
-              alignment: AlignmentDirectional.topEnd,
-              child: GestureDetector(
-                onTap: () {},
-                child: Icon(Icons.bookmark_rounded),
-              ),
-            ),
-            Align(
-              alignment: AlignmentDirectional.centerStart,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'COVID-19 Vaccine',
-                    maxLines: 2,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(right: 40.0),
-                    child: Text(
-                      'Official Public Service Announcement on Coronavirus from the WHO',
-                      softWrap: true,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.normal,
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  '01 Jan 2021',
+                  style: TextStyle(color: Colors.white),
+                ),
+                Container(
+                  width: 300.0,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'COVID-19 Vaccine',
+                        maxLines: 2,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
+                      Text(
+                        'Official Public Service Announcement on the Coronavirus by the WHO',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-            Align(
-              alignment: AlignmentDirectional.bottomEnd,
-              child: IconButton(
-                icon: Icon(Icons.arrow_forward_rounded),
-                onPressed: () {},
-              ),
+            Expanded(
+              child: SizedBox(),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                GestureDetector(
+                  onTap: () {},
+                  child: Icon(
+                    Icons.bookmark_outlined,
+                    color: Colors.white,
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(
+                    Icons.arrow_forward_rounded,
+                    color: Colors.white,
+                  ),
+                  onPressed: () {},
+                ),
+              ],
             ),
           ],
         ),
