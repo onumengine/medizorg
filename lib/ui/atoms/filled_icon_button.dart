79899a8856e4bc48201cc8744ebcc45e29e7cff9
onumengine <onumdev@gmail.com>
2021-01-01 16:14:10 +0100

    Feat(filled_icon_button.dart):Built the search buttton

--- lib/ui/atoms/filled_icon_button.dart
+++ lib/ui/atoms/filled_icon_button.dart
@@ -1,1 +1,22 @@
+import 'package:flutter/material.dart';
 
+class FilledIconButton extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Container(
+      height: 20.0,
+      width: 20.0,
+      decoration: BoxDecoration(
+        color: Colors.red,
+        borderRadius: BorderRadius.all(
+          Radius.circular(4.0),
+        ),
+      ),
+      child: GestureDetector(
+        child: Icon(Icons.search_rounded),
+        onTap: () {
+          print('pressed search');
+        },
+      ),
+    );
+  }
+}
--- lib/ui/molecules/search_bar.dart
+++ lib/ui/molecules/search_bar.dart
@@ -1,14 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:medizorg/ui/atoms/filled_icon_button.dart';
 
 class SearchBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
-      height: 30.0,
+      height: 40.0,
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.all(
           Radius.circular(8.0),
         ),
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Expanded(
+            child: TextField(
+              textAlignVertical: TextAlignVertical.bottom,
+              decoration: InputDecoration.collapsed(
+                hintText: 'Search doctors',
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: FilledIconButton(),
+          ),
+        ],
       ),
     );
   }
