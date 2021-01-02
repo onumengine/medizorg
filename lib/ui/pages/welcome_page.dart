7bcfa0d4606add792993f139e0a41cae5239670f
onumengine <onumdev@gmail.com>
2021-01-02 14:17:54 +0100

    QuickFix(welcome_page.dart):Formatted the Column widget list properly

--- lib/ui/pages/welcome_page.dart
+++ lib/ui/pages/welcome_page.dart
@@ -51,7 +51,7 @@ class _WelcomePageState extends State<WelcomePage> {
                     )
                   ],
                 ),
-              )
+              ),
             ],
           ),
         ),
