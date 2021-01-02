7d96ef94f154f6bb9d147377055a10d2736c44e5
onumengine <onumdev@gmail.com>
2021-01-02 15:04:22 +0100

    Feat(doctor_registration_page.dart):Built the [DoctorRegistrationPage]

---
+++ lib/ui/pages/doctor_registration_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DoctorRegistrationPage extends StatefulWidget {
+  _DoctorRegistrationPageState createState() => _DoctorRegistrationPageState();
+}
+
+class _DoctorRegistrationPageState extends State<DoctorRegistrationPage> {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Register As A Doctor'),
+      ),
+    );
+  }
+}
