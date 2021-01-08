38a07ff2ec2fc76062b42ecc62772467f5de1ad5
onumengine <onumdev@gmail.com>
2021-01-08 16:30:22 +0100

    Feat(sign_in_form.dart):Replaced the [Form] with regular [TextField]s

--- lib/ui/organisms/sign_in_form.dart
+++ lib/ui/organisms/sign_in_form.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:medizorg/ui/molecules/text_field.dart';
+import 'package:medizorg/ui/molecules/text_widget.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class SignInForm extends StatefulWidget {
   _SignInFormState createState() => _SignInFormState();
 }
 
 class _SignInFormState extends State<SignInForm> {
-  final _formKey = GlobalKey<FormState>();
   final _emailFieldKey = GlobalKey<FormState>();
   final _passwordFieldKey = GlobalKey<FormState>();
-  final _confirmPasswordFieldKey = GlobalKey<FormState>();
-  final _emailController = TextEditingController();
-  final _passwordController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  FirebaseAuth auth = FirebaseAuth.instance;
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -48,86 +55,116 @@ class _SignInFormState extends State<SignInForm> {
               style: TextStyle(color: Colors.black45),
             ),
           ),
-          Form(
-            key: _formKey,
-            child: Column(
-              children: <Widget>[
-                TextWidget(
-                  hintText: 'Email',
-                  key: _emailFieldKey,
-                  controller: _emailController,
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                TextWidget(
-                  hintText: 'Password',
-                  key: _passwordFieldKey,
-                  controller: _passwordController,
-                  obscureText: true,
-                  autocorrect: false,
-                ),
-                SizedBox(
-                  height: 4.0,
-                ),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: Text(
-                    'Forgot Password?',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                Container(
-                  height: 50.0,
-                  width: MediaQuery.of(context).size.width,
-                  decoration: BoxDecoration(
-                    color: Colors.blue,
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(16.0),
-                    ),
-                  ),
-                  child: TextButton(
-                    onPressed: () {
-                      Navigator.of(context).pushNamed('/home_page');
-                    },
-                    child: Text(
-                      'SIGN IN',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 16.0,
-                ),
-                Row(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      'Don\'t have an account?',
-                      style: TextStyle(color: Colors.black38),
-                    ),
-                    FlatButton(
-                      onPressed: () {
-                        Navigator.of(context).pushNamed('/sign_up_page');
-                      },
-                      child: Text(
-                        'SIGN UP',
-                        style: TextStyle(color: Colors.blue),
-                      ),
-                    )
-                  ],
-                ),
-              ],
+          TextWidget(
+            hintText: 'Email',
+            key: _emailFieldKey,
+            controller: emailController,
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          TextWidget(
+            hintText: 'Password',
+            key: _passwordFieldKey,
+            controller: passwordController,
+            obscureText: true,
+            autocorrect: false,
+          ),
+          SizedBox(
+            height: 4.0,
+          ),
+          Align(
+            alignment: Alignment.centerRight,
+            child: Text(
+              'Forgot Password?',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
             ),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Container(
+            height: 50.0,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              borderRadius: BorderRadius.all(
+                Radius.circular(16.0),
+              ),
+            ),
+            child: TextButton(
+              onPressed: () {
+                if (validateForm(
+                  emailController.text,
+                  passwordController.text,
+                )) {
+                  Navigator.of(context).pushNamed('/home_page');
+                } else {
+                  Scaffold.of(context).showSnackBar(SnackBar(
+                      content: Text("You have to enter valid content")));
+                }
+              },
+              child: Text(
+                'SIGN IN',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 16.0,
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Don\'t have an account?',
+                style: TextStyle(color: Colors.black38),
+              ),
+              FlatButton(
+                onPressed: () {
+                  Navigator.of(context).pushNamed('/sign_up_page');
+                },
+                child: Text(
+                  'SIGN UP',
+                  style: TextStyle(color: Colors.blue),
+                ),
+              )
+            ],
           ),
         ],
       ),
     );
+  }
+
+  bool emailIsValid(String email) {
+    return (email.contains("@"));
+  }
+
+  bool passwordIsValid(String password) {
+    return (password.length >= 8);
+  }
+
+  bool validateForm(String email, String password) {
+    return (emailIsValid(email) && passwordIsValid(password));
+  }
+
+  void createUser(String email, String password) async {
+    try {
+      await auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+    } catch (e) {
+      print(e);
+    }
   }
 }
