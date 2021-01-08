881e805b3d7db64e5793a9ec553e57066271742a
onumengine <onumdev@gmail.com>
2021-01-08 16:27:24 +0100

    Test(widget_test.dart):Wrote first widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:medizorg/ui/main.dart';
+import 'package:medizorg/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
