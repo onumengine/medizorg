ef9f104cf0053fb0c747f60126bcb0e9af9e36c6
onumengine <onumdev@gmail.com>
2021-01-09 16:51:49 +0100

    Feat(sign_up_form.dart):Added the createUserWithEmailAndPassword method

--- lib/ui/organisms/sign_up_form.dart
+++ lib/ui/organisms/sign_up_form.dart
@@ -1,18 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:medizorg/ui/molecules/text_widget.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class SignUpForm extends StatefulWidget {
   _SignUpFormState createState() => _SignUpFormState();
 }
 
 class _SignUpFormState extends State<SignUpForm> {
-  final _formKey = GlobalKey<FormState>();
   final _emailFieldKey = GlobalKey<FormState>();
   final _passwordFieldKey = GlobalKey<FormState>();
   final _confirmPasswordFieldKey = GlobalKey<FormState>();
   final emailTextController = TextEditingController();
   final passwordTextController = TextEditingController();
   final confirmPasswordTextController = TextEditingController();
+  FirebaseAuth auth = FirebaseAuth.instance;
 
   @override
   void dispose() {
@@ -58,87 +60,90 @@ class _SignUpFormState extends State<SignUpForm> {
               style: TextStyle(color: Colors.black45),
             ),
           ),
-          Form(
-            key: _formKey,
-            child: Column(
-              children: <Widget>[
-                TextWidget(
-                  hintText: 'Email',
-                  key: _emailFieldKey,
-                  controller: emailTextController,
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                TextWidget(
-                  hintText: 'Password',
-                  key: _passwordFieldKey,
-                  controller: passwordTextController,
-                  obscureText: true,
-                  autocorrect: false,
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                TextWidget(
-                  hintText: 'Confirm password',
-                  key: _confirmPasswordFieldKey,
-                  controller: confirmPasswordTextController,
-                  obscureText: true,
-                  autocorrect: false,
-                ),
-                SizedBox(
-                  height: 4.0,
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                Container(
-                  height: 50.0,
-                  width: MediaQuery.of(context).size.width,
-                  decoration: BoxDecoration(
-                    color: Colors.blue,
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(16.0),
-                    ),
-                  ),
-                  child: TextButton(
-                    onPressed: () {
-                      if (_emailFieldKey.currentState.validate()) {
-                        Navigator.of(context)
-                            .pushNamed('/doctor_registration_page');
-                      } else {
-                        Scaffold.of(context).showSnackBar(
-                          SnackBar(
-                            content: Text('You must meet all criteria!'),
-                          ),
-                        );
-                      }
-                    },
-                    child: Text(
-                      'SIGN UP',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 16.0,
-                ),
-                Align(
-                  alignment: AlignmentDirectional.bottomEnd,
-                  child: IconButton(
-                    icon: Icon(Icons.call_to_action_rounded),
-                    onPressed: () {
-                      Navigator.of(context)
-                          .pushNamed('/doctor_registration_page');
-                    },
-                  ),
-                ),
-              ],
+          TextWidget(
+            hintText: 'Email',
+            key: _emailFieldKey,
+            controller: emailTextController,
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          TextWidget(
+            hintText: 'Password',
+            key: _passwordFieldKey,
+            controller: passwordTextController,
+            obscureText: true,
+            autocorrect: false,
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          TextWidget(
+            hintText: 'Confirm password',
+            key: _confirmPasswordFieldKey,
+            controller: confirmPasswordTextController,
+            obscureText: true,
+            autocorrect: false,
+          ),
+          SizedBox(
+            height: 4.0,
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Container(
+            height: 50.0,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              borderRadius: BorderRadius.all(
+                Radius.circular(16.0),
+              ),
+            ),
+            child: TextButton(
+              onPressed: () {
+                createUser(
+                  emailTextController.text,
+                  passwordTextController.text,
+                );
+              },
+              child: Text(
+                'SIGN UP',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 16.0,
+          ),
+          Align(
+            alignment: AlignmentDirectional.bottomEnd,
+            child: IconButton(
+              icon: Icon(Icons.call_to_action_rounded),
+              onPressed: () {
+                Navigator.of(context).pushNamed('/doctor_registration_page');
+              },
             ),
           ),
         ],
       ),
     );
+  }
+
+  void createUser(String email, String password) async {
+    try {
+      UserCredential credential = await auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+    } catch (e) {
+      print(e);
+    }
   }
 }
