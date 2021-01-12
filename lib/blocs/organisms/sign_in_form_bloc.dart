251fe165bfa399b2ba1de5242d20ef082cac10d3
onumengine <onumdev@gmail.com>
2021-01-12 13:35:15 +0100

    Feat(sign_in_bloc.dart):added email and password validation logic

--- lib/blocs/organisms/sign_in_form_bloc.dart
+++ lib/blocs/organisms/sign_in_form_bloc.dart
@@ -15,4 +15,35 @@ class SignInFormBloc extends ChangeNotifier {
   TextEditingController get passwordController => _passwordController;
 
   FirebaseAuth auth = FirebaseAuth.instance;
+
+  bool emailIsEmpty(String email) {
+    return email.isEmpty;
+  }
+
+  bool emailIsValid(String email) {
+    return email.contains('@') && email.contains('.');
+  }
+
+  bool passwordIsLongEnough(String password) {
+    return password.length >= 8;
+  }
+
+  bool entriesAreValid(String email, String password) {
+    return !emailIsEmpty(email) &&
+        emailIsValid(email) &&
+        passwordIsLongEnough(password);
+  }
+
+  void signInUser(String email, String password) async {
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+    }
+  }
 }
