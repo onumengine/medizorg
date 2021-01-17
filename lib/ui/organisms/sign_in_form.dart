b8f7ba06cc8811061ecaab6a30b46bd2ab589bbb
onumengine <onumdev@gmail.com>
2021-01-17 13:51:41 +0100

    Fix(sign_in_form.dart):Made the form scrollable

--- lib/ui/organisms/sign_in_form.dart
+++ lib/ui/organisms/sign_in_form.dart
@@ -15,109 +15,120 @@ class _SignInFormState extends State<SignInForm> {
 
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Row(
-            children: <Text>[
-              Text(
-                TEXT_SIGN,
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                  fontSize: 30.0,
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 6,
+            ),
+            Row(
+              children: <Text>[
+                Text(
+                  TEXT_SIGN,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                    fontSize: 30.0,
+                  ),
                 ),
-              ),
-              Text(
-                TEXT_IN,
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.blue,
-                  fontSize: 30.0,
+                Text(
+                  TEXT_IN,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue,
+                    fontSize: 30.0,
+                  ),
                 ),
-              ),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 20.0),
-            child: Text(
-              TEXT_ENTER_CREDENTIALS,
-              style: TextStyle(color: Colors.black45),
+              ],
             ),
-          ),
-          TextWidget(
-            hintText: HINT_TEXT_EMAIL,
-            key: signInFormBloc.emailFieldKey,
-            controller: signInFormBloc.emailController,
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          TextWidget(
-            hintText: HINT_TEXT_PASSWORD,
-            key: signInFormBloc.passwordFieldKey,
-            controller: signInFormBloc.passwordController,
-            obscureText: true,
-            autocorrect: false,
-          ),
-          SizedBox(
-            height: 4.0,
-          ),
-          Align(
-            alignment: Alignment.centerRight,
-            child: Text(
-              TEXT_FORGOT_PASSWORD,
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          Container(
-            height: 50.0,
-            width: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadius.all(
-                Radius.circular(16.0),
-              ),
-            ),
-            child: TextButton(
-              onPressed: () {
-                signInFormBloc.getSuccessStatus(context);
-              },
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20.0),
               child: Text(
-                TEXT_SIGN_IN_UPPERCASE,
-                style: TextStyle(color: Colors.white),
+                TEXT_ENTER_CREDENTIALS,
+                style: TextStyle(color: Colors.black45),
               ),
             ),
-          ),
-          SizedBox(
-            height: 16.0,
-          ),
-          Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                TEXT_DONT_HAVE_AN_ACCOUNT,
-                style: TextStyle(color: Colors.black38),
-              ),
-              FlatButton(
-                onPressed: () {
-                  Navigator.of(context).pushNamed(ROUTE_SIGN_UP_PAGE);
+            TextWidget(
+              hintText: HINT_TEXT_EMAIL,
+              key: signInFormBloc.emailFieldKey,
+              controller: signInFormBloc.emailController,
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            TextWidget(
+              hintText: HINT_TEXT_PASSWORD,
+              key: signInFormBloc.passwordFieldKey,
+              controller: signInFormBloc.passwordController,
+              obscureText: true,
+              autocorrect: false,
+            ),
+            SizedBox(
+              height: 4.0,
+            ),
+            Align(
+              alignment: Alignment.centerRight,
+              child: GestureDetector(
+                onTap: () {
+                  signInFormBloc.openPage(
+                      ROUTE_PASSWORD_RECOVERY_PAGE, context);
                 },
                 child: Text(
-                  TEXT_SIGN_UP_UPPERCASE,
-                  style: TextStyle(color: Colors.blue),
+                  TEXT_FORGOT_PASSWORD,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              )
-            ],
-          ),
-        ],
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Container(
+              height: 50.0,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(16.0),
+                ),
+              ),
+              child: TextButton(
+                onPressed: () {
+                  signInFormBloc.getSuccessStatus(context);
+                },
+                child: Text(
+                  TEXT_SIGN_IN_UPPERCASE,
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 16.0,
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  TEXT_DONT_HAVE_AN_ACCOUNT,
+                  style: TextStyle(color: Colors.black38),
+                ),
+                FlatButton(
+                  onPressed: () {
+                    Navigator.of(context).pushNamed(ROUTE_SIGN_UP_PAGE);
+                  },
+                  child: Text(
+                    TEXT_SIGN_UP_UPPERCASE,
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
