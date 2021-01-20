912e26b5ee4720c202b98d9db6169845c9dab4f4
onumengine <onumdev@gmail.com>
2021-01-20 14:16:02 +0100

    Feat(video_chat_page.dart):Created the  [VideoChatPage]

---
+++ lib/ui/pages/video_chat_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class VideoChatPage extends StatefulWidget {
+  _VideoChatPageState createState() => _VideoChatPageState();
+}
+
+class _VideoChatPageState extends State<VideoChatPage> {
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
