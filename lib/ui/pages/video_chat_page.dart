1911b8b58ab38ca9565ed8fbbdbfae83e74f6115
onumengine <onumdev@gmail.com>
2021-01-20 14:45:52 +0100

    Feat(video_chat_page_bloc.dart):Created a bloc for the [VideoChatPage]

--- lib/ui/pages/video_chat_page.dart
+++ lib/ui/pages/video_chat_page.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:agora_rtc_engine/rtc_engine.dart';
+import 'package:agora_rtc_engine/rtc_local_view.dart' as RtcLocalView;
+import 'package:agora_rtc_engine/rtc_remote_view.dart' as RtcRemoteView;
 
 class VideoChatPage extends StatefulWidget {
+  final String channelName;
+
+  /*
+   This variable stands for the type of client that is joining the video chat. The client can either be the host or a guest
+   If the client is the host, the value of this variable will be ClientRole.Broadcaster and if the  client is a guest,
+   the value of this variable will be ClientRole.Audience
+  */
+  final ClientRole clientRole;
+
+  VideoChatPage({Key key, this.channelName, this.clientRole});
+
   _VideoChatPageState createState() => _VideoChatPageState();
 }
 
