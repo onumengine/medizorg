9606bec5c8525172a8521596b46c855bb34189f6
onumengine <onumdev@gmail.com>
2021-03-06 04:32:21 +0100

    created the quickblox chat page

--- lib/auth_module/authenticator.dart
+++ lib/auth_module/authenticator.dart
@@ -38,4 +38,5 @@ class Authenticator {
       print(e);
     }
   }
+
 }
---
+++ lib/ui/pages/quickblox_video_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class BloxChatPage extends StatefulWidget {
+  @override
+  _BloxChatPageState createState() => _BloxChatPageState();
+}
+
+class _BloxChatPageState extends State<BloxChatPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
